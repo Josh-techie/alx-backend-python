707cfd3a3bf7713570ad081804c2355e82a7fd04
Josh-Techie <youse2002@outlook.fr>
2024-01-17 11:29:25 +0100

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,9 @@
-# Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string
+#!/usr/bin/env python3
+"""
+Contains a single function that concatenates two strings.
+"""
 
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
